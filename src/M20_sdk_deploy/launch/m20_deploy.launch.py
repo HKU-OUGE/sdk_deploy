6d9f4cbe5b3a4e9dc763caec6a710769d9059d1c
唐天扬 <ttyaigsw@gmail.com>
2026-05-11 18:24:57 +0800

    feat(height_scan): add height_scan_node to m20_deploy launch file

--- src/M20_sdk_deploy/launch/m20_deploy.launch.py
+++ src/M20_sdk_deploy/launch/m20_deploy.launch.py
@@ -17,5 +17,18 @@ def generate_launch_description():
             executable='lidar_relay',
             name='lidar_relay_node',
             output='screen'
-        )
+        ),
+        Node(
+            package='m20_sdk_deploy',
+            executable='height_scan_node',
+            name='height_scan_node',
+            output='screen',
+            parameters=[{
+                'update_rate_hz': 20.0,
+                'min_imu_age_s': 0.1,
+                'pointcloud_topic': '/LIDAR_POINT_CLOUD_MERGED',
+                'imu_topic': '/IMU_DATA',
+                'output_topic': '/scan/height_features_array',
+            }],
+        ),
     ])
